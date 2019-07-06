a436bb3ce4ddcab53a5f58c8724144a01d5b0a6b
Logan Gorence <logan@gorence.io>
2019-07-05 22:22:51 -0700

    Add environment variable support.

--- infrastructure/sequelize.js
+++ infrastructure/sequelize.js
@@ -1,7 +1,14 @@
 let Sequelize = require("sequelize");
 
-const sequelize = new Sequelize('owntracks_endpoint', 'postgres', '', {
-    host: 'localhost',
+let dbName = process.env.DB_NAME || "owntracks_endpoint";
+let dbUser = process.env.DB_USER || "postgres";
+let dbPass = process.env.DB_PASS || "";
+let dbHost = process.env.DB_HOST || "localhost";
+let dbPort = process.env.DB_PORT || 5432;
+
+const sequelize = new Sequelize(dbName, dbUser, dbPass, {
+    host: dbHost,
+    port: dbPort,
     dialect: 'postgres',
     pool: {
         max: 5,
